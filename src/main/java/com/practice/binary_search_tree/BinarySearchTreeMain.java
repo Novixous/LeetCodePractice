307690e2e0eed27005d7969608652c722a0b3336
Khang Le <anonymousvhb@gmail.com>
2021-12-28 19:02:34 +0700

    add remove

--- src/main/java/com/practice/binary_search_tree/BinarySearchTreeMain.java
+++ src/main/java/com/practice/binary_search_tree/BinarySearchTreeMain.java
@@ -11,21 +11,28 @@ public class BinarySearchTreeMain {
         // write your code here
         BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
         BinarySearchTreeNoRec<Integer> binarySearchTreeNoRec = new BinarySearchTreeNoRec<>();
-        List<Integer> inputs = Arrays.asList(5,6,4,12,1,7,13);
+        List<Integer> inputs = Arrays.asList(41, 20, 65, 11, 29, 32, 50, 91, 72, 99, 15, 14, 13, 70, 68, 66, 71, 69);
         inputs.stream().forEach(number -> {
             binarySearchTree.insert(number);
             binarySearchTreeNoRec.insert(number);
         });
 
-        Node<Integer> node = binarySearchTree.lookup(6);
+        Node<Integer> node = binarySearchTree.lookup(11);
         System.out.println(gson.toJson(node));
         System.out.println(binarySearchTree.toJson());
-        Node<Integer> nodeNoRec = binarySearchTreeNoRec.lookup(6);
+        Node<Integer> nodeNoRec = binarySearchTreeNoRec.lookup(11);
         System.out.println(gson.toJson(nodeNoRec));
         System.out.println(binarySearchTreeNoRec.toJson());
 
         System.out.println(gson.toJson(node).equals(gson.toJson(nodeNoRec)));
         System.out.println(binarySearchTree.toJson().equals(binarySearchTreeNoRec.toJson()));
-        
+
+        binarySearchTreeNoRec.remove(15);
+        binarySearchTreeNoRec.remove(72);
+        System.out.println(binarySearchTreeNoRec.toJson());
+        binarySearchTreeNoRec.remove(70);
+        System.out.println(binarySearchTreeNoRec.toJson());
+        binarySearchTreeNoRec.remove(65);
+        System.out.println(binarySearchTreeNoRec.toJson());
     }
 }
--- src/main/java/com/practice/binary_search_tree/BinarySearchTreeNoRec.java
+++ src/main/java/com/practice/binary_search_tree/BinarySearchTreeNoRec.java
@@ -45,6 +45,71 @@ public class BinarySearchTreeNoRec<T extends Comparable<T>> {
         return null;
     }
 
+    boolean remove(T value) {
+        if (this.root != null) {
+            Node<T> currentNode = this.root;
+            Node<T> parrentNode = null;
+            while (currentNode != null) {
+                if (value.compareTo(currentNode.getValue()) < 0) {
+                    parrentNode = currentNode;
+                    currentNode = currentNode.getLeft();
+                } else if (value.compareTo(currentNode.getValue()) > 0) {
+                    parrentNode = currentNode;
+                    currentNode = currentNode.getRight();
+                } else {
+                    //Option 1: no right child
+                    if (currentNode.getRight() == null) {
+                        if (parrentNode == null) {
+                            this.root = currentNode.getLeft();
+                        } else {
+                            if (currentNode.getValue().compareTo(parrentNode.getValue()) > 0) {
+                                parrentNode.setRight(currentNode.getLeft());
+                            } else if (currentNode.getValue().compareTo(parrentNode.getValue()) < 0) {
+                                parrentNode.setLeft(currentNode.getLeft());
+                            }
+                        }
+                    }
+                    //option 2: right child does not have left child
+                    else if (currentNode.getRight().getLeft() == null) {
+                        currentNode.getRight().setLeft(currentNode.getLeft());
+                        if (parrentNode == null) {
+                            this.root = currentNode.getRight();
+                        } else {
+                            if (currentNode.getValue().compareTo(parrentNode.getValue()) > 0) {
+                                parrentNode.setRight(currentNode.getRight());
+                            } else if (currentNode.getValue().compareTo(parrentNode.getValue()) < 0) {
+                                parrentNode.setLeft(currentNode.getRight());
+                            }
+                        }
+                    }
+                    //Option 3: Right child that has a left child
+                    else {
+                        Node<T> leftMost = currentNode.getRight().getLeft();
+                        Node<T> leftMostParent = currentNode.getRight();
+                        while (leftMost.getLeft() != null) {
+                            leftMostParent = leftMost;
+                            leftMost = leftMost.getLeft();
+                        }
+                        leftMostParent.setLeft(leftMost.getRight());
+                        leftMost.setLeft(currentNode.getLeft());
+                        leftMost.setRight(currentNode.getRight());
+                        if (parrentNode == null) {
+                            this.root = leftMost;
+                        } else {
+                            if (currentNode.getValue().compareTo(parrentNode.getValue()) > 0) {
+                                parrentNode.setRight(leftMost);
+                            } else if (currentNode.getValue().compareTo(parrentNode.getValue()) < 0) {
+                                parrentNode.setLeft(leftMost);
+                            }
+                        }
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     String toJson() {
         Gson gson = new Gson();
         return gson.toJson(this.root);
